393c8e8be9ba0088b767e1b1bcf88f17b321f5dd
subandi12-dev <subandiahmad039@gmail.com>
2026-06-26 22:49:58 +0800

    git commit -m "fix: perbaikan error HTTP 500 akibat session tidak dimuat
    Penyebab utama: library session tidak dimuat sehingga
    \$this->session bernilai null dan melempar exception
    pada Auth::index() di baris 11.
    
    Perubahan:
    - application/config/autoload.php: menambahkan 'session' pada
      \$autoload['libraries'] agar session selalu tersedia
    - application/controllers/Auth.php: load session secara eksplisit
      di __construct() sebagai pengaman tambahan
    - application/config/database.php: menyesuaikan konfigurasi
      koneksi database kos_db pada localhost
    - index.php: penyesuaian pelaporan error untuk kompatibilitas
      PHP 8.x terkait deprecation warning dynamic property"

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,7 @@
 class Auth extends CI_Controller {
     public function __construct() {
         parent::__construct();
+        $this->load->library('session');  
         $this->load->model('User_model');
         $this->load->library('form_validation');
     }
